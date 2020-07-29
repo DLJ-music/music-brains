12d979f7fe3132db8754415978e9f08964cc7f17
JoLeaper <joleap123@gmail.com>
2020-07-29 14:52:52 -0700

    snapshot tests for presentational components written

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ArtistList from '../artists/ArtistList'
 
 export default function App() {
-  const artists = [{ id: 'stjkstkjlljst', name: 'Jackson Michael', type: 'Group', score: 87}]
+  const artists = [{ id: 'stjkstkjlljst', name: 'Jackson Michael', type: 'Group', score: 87 }]
   return <ArtistList artists={artists}/>;
 }
   
