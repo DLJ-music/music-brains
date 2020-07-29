eaaf709249d809b2930ca2b2414969a24c7fddbd
JoLeaper <joleap123@gmail.com>
2020-07-29 15:28:04 -0700

    structured maincontainer

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Search from '../components/search/Search'
+import ArtistList from '../components/artists/ArtistList'
 
 const MainContainer = () => {
+  // useSearchHook
+  const [searchQuery, setSearchQuery] = useState('')
+  useEffect(() => {
+    // mount artist list, no search query
+  }, [])
+
+  // useArtistList
+  const [artists, setArtists] = useState([])
+  useEffect(() => {
+    // fetch initial data, no search params
+    // setArtists to the received data
+  }, [])
   return (
     <div>
-      
+      <Search handleChange={handleChange} handleSearch={handleSearch} />
+      <ArtistList artists={artists}/>
     </div>
   )
 }
